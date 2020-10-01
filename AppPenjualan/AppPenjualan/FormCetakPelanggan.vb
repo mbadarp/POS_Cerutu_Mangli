560d32bbdfd78ae72a19052e961b132bf426a79a
egakustianpratama99 <47926994+egakustianpratama99@users.noreply.github.com>
2020-10-01 12:27:11 +0700

    .

---
+++ AppPenjualan/AppPenjualan/FormCetakPelanggan.vb
@@ -0,0 +1,6 @@
+﻿Public Class FormCetakPelanggan
+
+    Private Sub CrystalReportViewer1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CRP.Load
+        CRP.ReportSource = "..\..\CRPelanggan.rpt"
+    End Sub
+End Class
--- AppPenjualan/AppPenjualan/FormPelanggan.vb
+++ AppPenjualan/AppPenjualan/FormPelanggan.vb
@@ -5,10 +5,14 @@ Public Class FormPelanggan
         TextBox2.Text = ""
         TextBox3.Text = ""
         TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
         TextBox1.Enabled = False
         TextBox2.Enabled = False
         TextBox3.Enabled = False
         TextBox4.Enabled = False
+        TextBox5.Enabled = False
+        TextBox6.Enabled = False
 
         Button1.Enabled = True
         Button2.Enabled = True
@@ -32,6 +36,103 @@ Public Class FormPelanggan
         TextBox2.Enabled = True
         TextBox3.Enabled = True
         TextBox4.Enabled = True
-       
+        TextBox5.Enabled = True
+        TextBox6.Enabled = True
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Button1.Text = "Input" Then
+            Button1.Text = "Simpan"
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Text = "Batal"
+            Call SiapIsi()
+            TextBox1.Enabled = False
+            TextBox2.Focus()
+
+        Else
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Or TextBox5.Text = "" Or TextBox6.Text = "" Then
+                MsgBox("Silahkan isi semua data")
+            Else
+                Call Koneksi()
+                Dim InputData As String = "insert into tbl_pelanggan values('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & TextBox4.Text & "','" & TextBox5.Text & "','" & TextBox6.Text & "')"
+                Cmd = New OdbcCommand(InputData, Conn)
+                Cmd.ExecuteNonQuery()
+                MsgBox("Input Data Berhasil")
+                Call KondisiAwal()
+
+            End If
+        End If
+    End Sub
+
+    Private Sub Label5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If Button2.Text = "Edit" Then
+            Button2.Text = "Simpan"
+            Button1.Enabled = False
+            Button3.Enabled = False
+            Button4.Text = "Batal"
+            Call SiapIsi()
+        Else
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Or TextBox5.Text = "" Then
+                MsgBox("Silahkan isi semua data")
+            Else
+                Call Koneksi()
+                Dim UpdateData As String = "Update tbl_pelanggan set nama_pelangggan='" & TextBox2.Text & "',alamat='" & TextBox3.Text & "',deskripsi='" & TextBox4.Text & "',no_hp='" & TextBox5.Text & "' where kodepelanggan='" & TextBox1.Text & "'"
+                Cmd = New OdbcCommand(UpdateData, Conn)
+                Cmd.ExecuteNonQuery()
+                MsgBox("Update data berhasil")
+                Call KondisiAwal()
+
+            End If
+        End If
+    End Sub
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = Chr(13) Then
+            Call Koneksi()
+            Cmd = New OdbcCommand("Select * From tbl_pelanggan where kodepelanggan='" & TextBox1.Text & "'", Conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+            If Not Rd.HasRows Then
+                MsgBox("Kode pelanggan tidak ada")
+            Else
+                TextBox1.Text = Rd.Item("kodepelanggan")
+                TextBox2.Text = Rd.Item("nama_pelanggan")
+                TextBox3.Text = Rd.Item("alamat")
+                TextBox4.Text = Rd.Item("deskripsi")
+                TextBox5.Text = Rd.Item("no_hp")
+
+            End If
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If Button3.Text = "Hapus" Then
+            Button3.Text = "Delete"
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button4.Text = "Batal"
+            Call SiapIsi()
+        Else
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Or TextBox5.Text = "" Then
+                MsgBox("Silahkan isi semua data")
+            Else
+                Call Koneksi()
+                Dim HapusData As String = "Delete from tbl_pelanggan where kodepelanggan='" & TextBox1.Text & "'"
+                Cmd = New OdbcCommand(HapusData, Conn)
+                Cmd.ExecuteNonQuery()
+                MsgBox("Hapus data berhasil")
+                Call KondisiAwal()
+
+            End If
+        End If
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        FormCetakPelanggan.ShowDialog()
     End Sub
 End Class
